750225b93970a7229f18a5d8411951d93dcb6ad5
Twilight Goblin <ayushmanjunathas@gmail.com>
2025-11-25 20:25:00 +0530

    updating skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Hero from './components/hero/hero'
 import Navbar from './components/navbar/navbar'
 import { ScrollTimelineExample } from './components/experience/experience'
+import Skills from './components/skills/skills'
 
 const App = () => {
   return (
     <div className="bg-black min-h-screen">
       <Navbar />
       <Hero />
+      <div className="section-separator"></div>
       <section id="experience">
         <ScrollTimelineExample />
       </section>
+      <div className="section-separator"></div>
+      <Skills />
     </div>
   )
 }
